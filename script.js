4cca7d6c1b77c8c8c73769b25565e0df81ddcf10
james <james@devpipeline.com>
2023-03-04 16:47:35 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+Assignments 3/2/2023
+
+Data Dump Formatting -
+  You have been given a raw data dump that is structured as an array of objects. The object's keys are companies, and the values are arrays of emails followed by a 3 digit department number. I need you to write a program that will go through the data, print the emails for each company and what department number that email belongs to. The format could look something like:
+  Company One
+  email: email@email.com, dept: 123
+  email: email@email.com, dept: 123
+
+  Company Two
+  email: email@email.com, dept: 123
+  email: email@email.com, dept: 123
+*/
